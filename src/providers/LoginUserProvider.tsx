055ab20540a46e3ebd45bd105b363f6ca65f9371
AkieMaezawa <98136950+AkieMaezawa@users.noreply.github.com>
2022-01-28 18:40:17 +0900

    Initial commit

---
+++ src/providers/LoginUserProvider.tsx
@@ -0,0 +1,27 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState
+} from "react";
+import { User } from "../types/api/User";
+type LoginUser = User & { isAdmin: boolean };
+
+export type LoginUserContextType = {
+  loginUser: LoginUser | null;
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>;
+};
+
+export const LoginUserContext = createContext({} as LoginUserContextType);
+
+export const LoginUserProvider = (props: { children: ReactNode }) => {
+  const { children } = props;
+  const [loginUser, setLoginUser] = useState<User | null>();
+
+  return (
+    <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+      {children}
+    </LoginUserContext.Provider>
+  );
+};
